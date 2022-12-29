c8e24c0a30f96ab3ca4106061c1aacbf2aa9ddda
LifeToHell <120266818+LifeToHell@users.noreply.github.com>
2022-12-30 00:43:53 +0400

    Add files via upload

--- task3.1.cs
+++ task3.1.cs
@@ -1,26 +1,34 @@
-﻿//Задача 19
+﻿//Задача 21
 
-//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
-//14212 -> нет
+//A (3,6,8); B (2,1,-7), -> 15.84
 
-//12821 -> да
-
-//23432 -> да
-
+//A (7,-5, 0); B (1,-1,9) -> 11.53
 
 Console.Clear();
 
-Console.Write("Введи пятизначное число:");
-int number = Convert.ToInt32(Console.ReadLine());
-string numbersize = Convert.ToString(number);
-if (numbersize.Length == 5){
-if (numbersize[0] == numbersize[4] || numbersize[1] == numbersize[3]){
-Console.Write(numbersize + "-" + "является палиндромом");
+int x1 = Coordinate("x", "A");
+int y1 = Coordinate("y", "A");
+int z1 = Coordinate("z", "A");
+int x2 = Coordinate("x", "B");
+int y2 = Coordinate("y", "B");
+int z2 = Coordinate("z", "B");
+
+int Coordinate(string number, string dot)
+{
+    Console.Write($"Введите координату {number} точки {dot}: ");
+    return Convert.ToInt16(Console.ReadLine());
 }
-else Console.Write(numbersize + "-" + "не является палиндромом");
+
+double Decision(double x1, double x2, 
+                double y1, double y2, 
+                double z1, double z2){
+  return Math.Sqrt(Math.Pow((x2-x1), 2) + 
+                   Math.Pow((y2-y1), 2) + 
+                   Math.Pow((z2-z1), 2));
 }
-else Console.Write("Неверное число");
 
+double segmentLength =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
 
-
+Console.WriteLine($"Длина отрезка  {segmentLength}");
